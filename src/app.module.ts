07922079f6575d0af513fcd24a75ad1880ecef64
Mr.Igor <iv_mirsoft@ukr.net>
2025-11-05 22:48:52 +0200

    logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,37 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
-import { LoggerModule } from 'nestjs-pino';
 import { QuestionsModule } from './questions/questions.module';
 import { GameSessionModule } from './gameSession/gameSession.module';
 import { GameInstanceModule } from './game-instance/game-instance.module';
+import { GameLoggerModule } from './common/logger/logger.module';
 
 @Module({
   imports: [
-    LoggerModule.forRoot({
-      pinoHttp: {
-        transport:
-          process.env.NODE_ENV !== 'production'
-            ? { target: 'pino-pretty', options: { colorize: true } }
-            : undefined,
-        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
-        autoLogging: true, // можеш увімкнути true, щоб логувало все
-        serializers: {
-          req(req) {
-            return {
-              method: req.method,
-              url: req.url,
-              ip: req.raw?.socket?.remoteAddress,
-            };
-          },
-          res(res) {
-            return {
-              statusCode: res?.statusCode,
-            };
-          },
-        },
-      },
-    }),
+    GameLoggerModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
